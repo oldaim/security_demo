be2d26cb060a211f2652dd599119a1856a662119
Kimdongkyun <dgk0911@gmail.com>
2024-08-11 19:47:09 +0900

    [demo-2] CustomUsernamePasswordAuthenticationFilter 생성

---
+++ src/main/kotlin/org/toy/security_demo/config/security/session/CustomUsernamePasswordAuthenticationFilter.kt
@@ -0,0 +1,39 @@
+package org.toy.security_demo.config.security.session
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import javax.naming.AuthenticationException
+
+class CustomUsernamePasswordAuthenticationFilter(
+    defaultFilterProcessesUrl: String?,
+    authenticationManager: AuthenticationManager?,
+    failureHandler: AuthenticationFailureHandler?
+): AbstractAuthenticationProcessingFilter(defaultFilterProcessesUrl, authenticationManager) {
+
+    override fun attemptAuthentication(request: HttpServletRequest?, response: HttpServletResponse?): Authentication {
+        TODO("Username 과 Password 를 통해 Authentication 과정 작성 필요")
+    }
+
+    override fun successfulAuthentication(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        chain: FilterChain?,
+        authResult: Authentication?
+    ) {
+        // ContextHolder 에서 Empty Context 생성후 Authentication 객체를 넣어준다.
+        super.successfulAuthentication(request, response, chain, authResult)
+    }
+
+    override fun setAuthenticationFailureHandler(failureHandler: AuthenticationFailureHandler?) {
+        TODO("Authentication 실패시 처리할 로직 작성 필요")
+    }
+
+
+}
+
