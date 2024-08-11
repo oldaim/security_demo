93dcd7b149cad16858258e6ae85700acea97eddd
Kimdongkyun <dgk0911@gmail.com>
2024-08-11 19:52:08 +0900

    [demo-2] CustomUsernamePasswordAuthenticationFilter 수정 - setMethod 를 통하여 FailureHandler 다루도록 수정

--- src/main/kotlin/org/toy/security_demo/config/security/session/CustomUsernamePasswordAuthenticationFilter.kt
+++ src/main/kotlin/org/toy/security_demo/config/security/session/CustomUsernamePasswordAuthenticationFilter.kt
@@ -7,13 +7,10 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import javax.naming.AuthenticationException
 
 class CustomUsernamePasswordAuthenticationFilter(
-    defaultFilterProcessesUrl: String?,
-    authenticationManager: AuthenticationManager?,
-    failureHandler: AuthenticationFailureHandler?
+    defaultFilterProcessesUrl: String,
+    authenticationManager: AuthenticationManager,
 ): AbstractAuthenticationProcessingFilter(defaultFilterProcessesUrl, authenticationManager) {
 
     override fun attemptAuthentication(request: HttpServletRequest?, response: HttpServletResponse?): Authentication {
@@ -29,11 +26,5 @@ class CustomUsernamePasswordAuthenticationFilter(
         // ContextHolder 에서 Empty Context 생성후 Authentication 객체를 넣어준다.
         super.successfulAuthentication(request, response, chain, authResult)
     }
-
-    override fun setAuthenticationFailureHandler(failureHandler: AuthenticationFailureHandler?) {
-        TODO("Authentication 실패시 처리할 로직 작성 필요")
-    }
-
-
 }
 
